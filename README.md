cdd195c6f2b270363fe3e6018c8f72ca0155208c
hehangfighting <hehangdehao@outlook.com>
2017-07-12 23:35:22 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 #### React 脚手架
 > 基于antd开发的一个脚手架，致力于开发一个通用的后台系统脚手架。
+
+> 个人不希望项目配置搞的过于复杂难懂，而是希望各位根据实际业务来调整，所以很多配置化都会基于简单 & 灵活 & 扩展性高 为最基标准。
+
+
+#### 目录结构
+``` shell
+├── dist                          # 项目输出目录
+├── config                        # webpack配置文件
+│   ├── webpack.config.dev.js     # 开发环境配置
+│   └── webpack.config.prod.js    # 生产环境配置
+├── public                        # 公共资源
+├── src                           # 项目源码目录
+│ ├──├── components               # UI组件及UI相关方法
+│ │  ├── skin.less                # 全局样式
+│ │  └── vars.less                # 全局样式变量
+│ ├── route.js                    # 路由配置
+│ ├── index.js                    # 入口文件js
+│ └── index.html                  # 载入点
+├── .babelrc      
+├── .eslintrc.js                
+├── .gitignore
+├── package.json
+├── README.md
+└── yarn.lock
+```
---
+++ webpack.config.dev.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack'); // 引入webpack，使用webpack内置插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NODE_ENV = process.env.NODE_ENV || 'dev';
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'react-hot-loader/patch',
+    './index.jsx'
+  ],
+  output: {
+    publicPath: '/',
+    path: path.join(__dirname, 'dist'),
+    filename: 'bunlde.[hash].js'
+  },
+  externals: {
+  // jquery: 'window.jQuery'
+  },
+  module: {
+    loaders: [
+      {
+        // 也可以多文件后缀：/\.(js|jsx)$/
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        loader: 'file-loader?name=img/[hash].[ext]'
+      }, {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'resolve-url-loader',
+          { loader: 'sass-loader', options: { sourceMap: true } }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    // 开启服务器的模块热替换（HMR）
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    // 输出文件的路径
+    publicPath: '/',
+    // 和上文output的"publicPath"值保持一致
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'react-antd-swissgear',
+      template: 'src/index.html',
+      filename: 'index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    // 开启全局的模块热替换（HMR）
+    new webpack.NamedModulesPlugin(),
+    // 当模块热替换（HMR）时在浏览器控制台输出对用户更友好的模块名字信息
+    new webpack.DefinePlugin({
+      NODE_ENV
+    })
+  ]
+};
